61048d82de35dd49cafef1271cf8ce40d0fb2fc3
Courtney Mahida <mahida.guadalupe@gmail.com>
2015-12-09 16:48:45 -0500

    added thumbnails to episode and podcast

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -1,4 +1,6 @@
 class EpisodesController < ApplicationController
+	before_action :authenticate_podcast!, except: [:show]
+	before_filter :require_permission
 	before_action :find_podcast
 	before_action :find_episode, only: [:show, :edit, :update, :destroy]
 
@@ -38,7 +40,7 @@ class EpisodesController < ApplicationController
 	private
 
 		def episode_params
-			params.require(:episode).permit(:title, :description)
+			params.require(:episode).permit(:title, :description, :thumbnail)
 		end
 
 		def find_podcast
@@ -47,5 +49,12 @@ class EpisodesController < ApplicationController
 
 		def find_episode
 			@episode = Episode.find(params[:id])
+		end
+
+		def require_permission
+			@podcast = Podcast.find(params[:podcast_id])
+			if current_podcast != @podcast
+				redirect_to root_path, notice: "Sorry, you're not allowed to view that page."
+			end
 		end
 end
--- app/models/podcast.rb
+++ app/models/podcast.rb
@@ -7,4 +7,7 @@ class Podcast < ActiveRecord::Base
   validates :title, presence: true, length: {maximum: 100}
 
   has_many :episodes
+
+  has_attached_file :thumbnail, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :thumbnail, content_type: /\Aimage\/.*\Z/
 end

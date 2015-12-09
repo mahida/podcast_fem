34315b65bda562b663fa8072df761ca3241f3558
Courtney Mahida <mahida.guadalupe@gmail.com>
2015-12-09 12:11:37 -0500

    added episode controller and views

--- app/models/podcast.rb
+++ app/models/podcast.rb
@@ -5,4 +5,6 @@ class Podcast < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   validates :title, presence: true, length: {maximum: 100}
+
+  has_many :episodes
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   devise_for :podcasts
   root 'welcome#index'
 
-  resources :podcasts, only: [:index, :show]
+  resources :podcasts, only: [:index, :show] do
+    resources :episodes
+  end
 
   
   # The priority is based upon order of creation: first created -> highest priority.

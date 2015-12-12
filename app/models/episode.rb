d43c34a4ac4b9eaac47221e3b016a787df76190c
Courtney Mahida <mahida.guadalupe@gmail.com>
2015-12-12 14:50:36 -0500

    added mp3 to episodes and styles

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -3,4 +3,8 @@ class Episode < ActiveRecord::Base
 
   has_attached_file :thumbnail, styles: { medium: "300x300#", thumb: "160x160#" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :thumbnail, content_type: /\Aimage\/.*\Z/
+
+  has_attached_file :mp3
+  validates_attachment :mp3, content_type: { content_type: ["audio/mpeg", "audio/mp3"] }, file_name: { matches: [/mp3\Z/] }
+
 end
